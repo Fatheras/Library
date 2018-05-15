65ca2fb7bca9db866c05ebf3a4ec4d424d304526
Fatheras <kosras.96@gmail.com>
2018-05-15 16:35:24 +0300

    v.0

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = function(app) {
+  app.get('/', function(req, res) {
+    res.render('pages/index');
+  });
+
+  app.get('/about', function(req, res) {
+    res.render('pages/about');
+  });
+  app.get('/table', function(req, res) {
+    res.render('pages/table');
+  });
+  app.get('/signup', function(req, res) {
+    res.render('pages/signup');
+  });
+  app.get('/signin', function(req, res) {
+    res.render('pages/signin');
+  });
+};
